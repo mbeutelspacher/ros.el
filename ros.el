62dd01fff1e33c6fe7099cb02548b1ab109540b9
Max Beutelspacher <max.beutelspacher@mailbox.org>
2019-05-21 20:17:50 +0200

    add package description

--- ros.el
+++ ros.el
@@ -24,7 +24,10 @@
 
 ;;; Commentary:
 
-;; A package to ease the interaction ROS nodes and the development of ROS software
+;; A package to ease the interaction ROS nodes and the development of ROS software.
+;; This includes the interaction with the catkin build system, interactively exploring ROS nodes,
+;; messages, topics, and services, as well as some convenience functions when coding for ROS in
+;; C++ and Python.
 
 ;;; Code:
 (require 'dired)
